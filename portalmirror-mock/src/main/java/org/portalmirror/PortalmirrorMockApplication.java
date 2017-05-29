358ef03e8887c659449f16483b12102b60a91c46
Artur Karwowski <artur.karwowski@mailplus.pl>
2017-05-29 20:55:22 +0100

    jwt service mock project now includes other mocks

--- portalmirror-mock/src/main/java/org/portalmirror/PortalmirrorMockApplication.java
+++ portalmirror-mock/src/main/java/org/portalmirror/PortalmirrorMockApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PortalmirrorWebsocketJwtMockApplication {
+public class PortalmirrorMockApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PortalmirrorWebsocketJwtMockApplication.class, args);
+		SpringApplication.run(PortalmirrorMockApplication.class, args);
 	}
 }
--- portalmirror-mock/src/test/java/org/portalmirror/PortalmirrorMockApplicationTests.java
+++ portalmirror-mock/src/test/java/org/portalmirror/PortalmirrorMockApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PortalmirrorWebsocketJwtMockApplicationTests {
+public class PortalmirrorMockApplicationTests {
 
 	@Test
 	public void contextLoads() {
